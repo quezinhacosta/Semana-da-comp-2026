bf94da3a456f71dd0ee82545ed1ef7bddccd5501
Quezia Costa <154010743+quezinhacosta@users.noreply.github.com>
2026-05-05 13:19:00 +0000

    menos estresse

--- create/app/page.js
+++ create/app/page.js
@@ -1,44 +1,41 @@
 import Image from "next/image";
-import "./globals.css"; 
+import "./globals.css";
 
 export default function Home() {
   return (
     <main className="main-container">
       <div className="creative-card">
         
-        <header className="header-section">
-          <h1>Quezia Costa</h1>
-          <p className="subtitle"><strong>Bem-vindos ao último dia da semana da tecnologia!</strong></p>
-          <p className="description">Entenda o processo de aplicação e como usar a IA a seu favor.</p>
-        </header>
+        <div className="header-title">Quezia Costa</div>
+        
+        <p className="header-text">
+          <strong>Bem-vindos ao último dia da semana da tecnologia!</strong>
+        </p>
+        
+        <p className="header-text">
+          Entenda o processo de aplicação e como usar a IA a seu favor.
+        </p>
 
-        <section className="profile-section">
-          <div className="photo-wrapper">
-            <img 
-              src="/minha-foto.png" 
-              alt="Quezia Costa" 
-            />
-          </div>
+        <div className="profile-wrapper">
           
-          <ul className="info-list">
+          
+          <ul className="custom-list">
             <li>Futura cientista da computação</li>
             <li>Bolsista PIBIC: Apps gamificados para alfabetização</li>
             <li>Estagiária Fullstack com foco em IA no CIA/FASA</li>
-            <li>Mãe de gata tricolor </li>
-            <li>Liderança no projeto Padawan</li>
+            <li>Mãe de gata tricolor</li>
+            <li>Participante da liderança no projeto de extensão Padawan</li>
           </ul>
-        </section>
-
-        <div className="button-wrapper">
-          <a 
-            href="LINK_DO_SEU_REPOSITORIO_AQUI" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="btn-desafio"
-          >
-             Veja o desafio
-          </a>
         </div>
+
+        <a 
+          href="LINK_DO_SEU_REPOSITORIO_AQUI" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="btn-action"
+        >
+          Veja o desafio
+        </a>
       </div>
     </main>
   );
